192ca9a7a96623f154bc8659b8c64a99ce969a4a
mangesh9903 <mangeshb9903@gmail.com>
2022-03-21 17:06:31 +0530

    Welcome to Find Maximum Number Using Generics

---
+++ src/main/java/com/bridgelabz/generics/FindMaxValue.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.generics;
+
+public class FindMaxValue {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Find Maximum Number Using Generics.");
+    }
+}
