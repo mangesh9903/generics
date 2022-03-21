25b04de19b1f612c6e589f8c3ac34ce82b0e59c8
mangesh9903 <mangeshb9903@gmail.com>
2022-03-21 22:17:09 +0530

    ADDED: Test case for Maximum Integer from given Three Integer.

--- src/main/java/com/bridgelabz/generics/FindMaxValue.java
+++ src/main/java/com/bridgelabz/generics/FindMaxValue.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.generics;
 
 public class FindMaxValue {
-    public static void main(String[] args) {
-        System.out.println(" Welcome to Find Maximum Number Using Generics.");
+
+    public Integer findMaximumValue(Integer number1, Integer number2, Integer number3) {
+        Integer maximumValue = number1;
+        if (number2.compareTo(maximumValue) > 0) {
+            maximumValue = number2;
+        }
+        if (number3.compareTo(maximumValue) > 0) {
+            maximumValue = number3;
+        }
+        return maximumValue;
     }
 }
