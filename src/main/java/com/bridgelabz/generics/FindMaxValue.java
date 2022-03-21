081852f18c49bce0d3ed337fb4a9873e4b48f94e
mangesh9903 <mangeshb9903@gmail.com>
2022-03-21 22:33:55 +0530

    ADDED: Test case for Maximum Float from given Three Float.

--- src/main/java/com/bridgelabz/generics/FindMaxValue.java
+++ src/main/java/com/bridgelabz/generics/FindMaxValue.java
@@ -12,4 +12,15 @@ public class FindMaxValue {
         }
         return maximumValue;
     }
+
+    public Float findMaximumFloatValue(Float number1, Float number2, Float number3) {
+        Float maximumValue = number1;
+        if (number2.compareTo(maximumValue) > 0) {
+            maximumValue = number2;
+        }
+        if (number3.compareTo(maximumValue) > 0) {
+            maximumValue = number3;
+        }
+        return maximumValue;
+    }
 }
--- src/test/java/com/bridgelabz/generics/FindMaxValueTest.java
+++ src/test/java/com/bridgelabz/generics/FindMaxValueTest.java
@@ -1,26 +1,59 @@
 package com.bridgelabz.generics;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 public class FindMaxValueTest {
     FindMaxValue findMaxValue = new FindMaxValue();
+
+    @BeforeClass
+    public static void before() {
+        System.out.println("Starting Test Cases.");
+    }
+
+    @AfterClass
+    public static void after() {
+        System.out.println("Ending Test Cases.");
+    }
+
     @Test
-    public void givenThreeIntegerNumber_shouldReturn_firstNumberIsMaximum(){
-        Integer actual = findMaxValue.findMaximumValue(100,20,30);
+    public void givenThreeIntegerNumber_shouldReturn_firstNumberIsMaximum() {
+        Integer actual = findMaxValue.findMaximumValue(100, 20, 30);
         Integer expected = 100;
         Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void givenThreeIntegerNumber_shouldReturn_secondNumberIsMaximum(){
-        Integer actual = findMaxValue.findMaximumValue(100,20,30);
+    public void givenThreeIntegerNumber_shouldReturn_secondNumberIsMaximum() {
+        Integer actual = findMaxValue.findMaximumValue(100, 20, 30);
         Integer expected = 100;
         Assert.assertEquals(expected, actual);
     }
+
     @Test
-   public void givenThreeIntegerNumber_shouldReturn_thirdNumberIsMaximum(){
-        Integer actual = findMaxValue.findMaximumValue(100,20,30);
+    public void givenThreeIntegerNumber_shouldReturn_thirdNumberIsMaximum() {
+        Integer actual = findMaxValue.findMaximumValue(100, 20, 30);
         Integer expected = 100;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenThreeFloatNumber_shouldReturn_firstNumberIsMaximum() {
+        Float actual = findMaxValue.findMaximumFloatValue(10.0F, 5.0F, 3.0F);
+        Float expected = 10.0F;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenThreeFloatNumber_shouldReturn_secondNumberIsMaximum() {
+        Float actual = findMaxValue.findMaximumFloatValue(5.0F, 10.0F, 3.0F);
+        Float expected = 10.0F;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenThreeFloatNumber_shouldReturn_thirdNumberIsMaximum() {
+        Float actual = findMaxValue.findMaximumFloatValue(3.0F, 5.0F, 10.0F);
+        Float expected = 10.0F;
         Assert.assertEquals(expected, actual);
     }
 }
